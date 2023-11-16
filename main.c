1209ad7294f69743b738ddea1ac04416fd656aa3
Meryeme08 <bouchritmeryeme@gmail.com>
2023-11-16 15:49:41 -0800

    up date

--- main.c
+++ main.c
@@ -1,45 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "monty.h"
-bus_t bus = {NULL, NULL, NULL, 0};
-/**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
-int main(int argc, char *argv[])
-{
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
-return (0);
+#define MAX_LINE_LENGTH 1024
+
+int main(int argc, char *argv[]) {
+    char content[MAX_LINE_LENGTH];
+    FILE *file;
+    stack_t *stack = NULL;
+    unsigned int counter = 0;
+    size_t len; /* Declare size_t here due to C90 rules */
+
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    if (!file) {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(content, MAX_LINE_LENGTH, file) != NULL) {
+        counter++;
+
+        /* Check for line length exceeding buffer */
+        if (content[MAX_LINE_LENGTH - 1] != '\0' && content[MAX_LINE_LENGTH - 1] != '\n') {
+            fprintf(stderr, "Error: Line %d exceeds maximum length\n", counter);
+            exit(EXIT_FAILURE);
+        }
+
+        len = strlen(content); /* Assign value to len after declarations */
+        if (len > 0 && content[len - 1] == '\n') {
+            content[len - 1] = '\0';
+        }
+
+        execute(content, &stack, counter, file);
+    }
+
+    fclose(file);
+    return 0;
 }
